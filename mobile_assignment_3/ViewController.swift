361aea410039551137425b5f4525cd9f18b7e306
sasalin777 <candysasame@gmail.com>
2021-12-16 13:49:43 -0800

    upload

---
+++ mobile_assignment_3/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  mobile_assignment_3
+//
+//  Created by Li Tzu Lin on 2021-12-15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func goToDoorway(_ sender: Any) {
+        
+        
+    }
+}
+
